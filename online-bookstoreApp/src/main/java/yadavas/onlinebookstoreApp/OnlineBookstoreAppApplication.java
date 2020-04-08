761e251cbe1cddc848c68c4f88a5b7266359c687
PrafullaPDauwar <synnexprafulla@.com>
2020-04-08 18:56:40 +0530

    First commit. creating back-end application configuring the properties and ready to development.

---
+++ online-bookstoreApp/src/main/java/yadavas/onlinebookstoreApp/OnlineBookstoreAppApplication.java
@@ -0,0 +1,14 @@
+package yadavas.onlinebookstoreApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookstoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookstoreAppApplication.class, args);
+		System.out.println("running");
+	}
+
+}
---
+++ online-bookstoreApp/src/test/java/yadavas/onlinebookstoreApp/OnlineBookstoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package yadavas.onlinebookstoreApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
