26d7c86c3b6cf4c1d8bbdb3743243f0ddf33151c
PrafullaPDauwar <prafulladauwar04@.com>
2020-04-15 01:06:03 +0530

    created Angular9 using project and completed feature 1.

--- online-bookstoreApp/src/main/java/yadavas/onlinebookstoreApp/repository/BookRepository.java
+++ online-bookstoreApp/src/main/java/yadavas/onlinebookstoreApp/repository/BookRepository.java
@@ -2,9 +2,11 @@ package yadavas.onlinebookstoreApp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import yadavas.onlinebookstoreApp.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel="books",path="books")
 public interface BookRepository extends JpaRepository<Book,Long> {
 
