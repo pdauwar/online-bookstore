7344d715c0697ec96e16b8fd3bd96dab4cf39ae1
PrafullaPDauwar <synnexprafulla@.com>
2020-04-16 01:13:33 +0530

    completed future 3 search by cat

--- book-app/src/app/services/book.service.ts
+++ book-app/src/app/services/book.service.ts
@@ -15,8 +15,9 @@ export class BookService {
 
   constructor(private httpClient: HttpClient ) { }
 
-  getBooks():Observable<Book[]>{
-   return this.httpClient.get<GetRespomseBooks>(this.baseUrl).pipe(
+  getBooks(theCategoryId:number):Observable<Book[]>{
+   const searchUrl=`${this.baseUrl}/search/categoryid?id=${theCategoryId}`;
+   return this.httpClient.get<GetRespomseBooks>(searchUrl).pipe(
    map(response =>response._embedded.books)
    );
   }
