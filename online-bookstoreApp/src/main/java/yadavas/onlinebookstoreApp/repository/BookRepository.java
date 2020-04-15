1a15d48db2ad9868a400736cebf06e739988e53b
PrafullaPDauwar <synnexprafulla@.com>
2020-04-16 01:12:47 +0530

    feature 3 started

--- online-bookstoreApp/src/main/java/yadavas/onlinebookstoreApp/repository/BookRepository.java
+++ online-bookstoreApp/src/main/java/yadavas/onlinebookstoreApp/repository/BookRepository.java
@@ -1,7 +1,11 @@
 package yadavas.onlinebookstoreApp.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import yadavas.onlinebookstoreApp.entity.Book;
@@ -10,4 +14,6 @@ import yadavas.onlinebookstoreApp.entity.Book;
 @RepositoryRestResource(collectionResourceRel="books",path="books")
 public interface BookRepository extends JpaRepository<Book,Long> {
 
+	@RestResource(path="categoryid")
+	Page<Book> findByCategoryId(@Param("id") Long id,Pageable pageable);
 }
